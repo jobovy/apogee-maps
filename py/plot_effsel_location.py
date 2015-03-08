9069938269032d716f52162e4f39205c4eae1f5d
Jo Bovy <jo.bovy@gmail.com>
2015-03-08 17:07:09 +0000

    apogee-maps: also plot the full jkz effective selection function

--- py/plot_effsel_location.py
+++ py/plot_effsel_location.py
@@ -12,7 +12,9 @@ from galpy.util import save_pickles, bovy_plot
 from matplotlib import rc, pyplot
 import mwdust
 import apogee.select.apogeeSelect
+from define_rcsample import get_rcsample
 _PLOTDIST= True
+_LW= 1.5
 def plot_effsel_location(location,plotname):
     # Setup selection function
     selectFile= '../savs/selfunc-nospdata.sav'
@@ -28,14 +30,20 @@ def plot_effsel_location(location,plotname):
     effselFile= '../savs/effselfunc-%i.sav' % location
     if not os.path.exists(effselFile):
         # Distances at which to calculate the effective selection function
-        distmods= numpy.linspace(7.,15.5,101)
+        distmods= numpy.linspace(7.,15.5,201)
         ds= 10.**(distmods/5-2.)
         # Setup default effective selection function
         gd= mwdust.Green15(filter='2MASS H',load_samples=True)
         apof= apogee.select.apogeeEffectiveSelect(apo,dmap3d=gd)
         sf_default= apof(location,ds)
+        # Also calculate for a sample of MH
+        data= get_rcsample()
+        MH= data['H0']-data['RC_DM']
+        MH= numpy.random.permutation(MH)[:1000]
+        sf_jkz= apof(location,ds,MH=MH)
+        # Go through the samples
         sf_samples= numpy.zeros((20,len(ds)))
-        if True:
+        if False:
             for ii in range(20):
                 # Swap in a sample for bestfit in the Green et al. (2015) dmap
                 gd._intps= numpy.zeros(len(gd._pix_info['healpix_index']),
@@ -52,12 +60,13 @@ def plot_effsel_location(location,plotname):
         marshall= mwdust.Marshall06(filter='2MASS H')
         apof= apogee.select.apogeeEffectiveSelect(apo,dmap3d=marshall)
         sf_marshall= apof(location,ds)
-        save_pickles(effselFile,distmods,sf_default,sf_samples,sf_zero,
-                     sf_drimmel,sf_marshall)
+        save_pickles(effselFile,distmods,sf_default,sf_jkz,sf_samples,
+                     sf_zero,sf_drimmel,sf_marshall)
     else:
         with open(effselFile,'rb') as savefile:
             distmods= pickle.load(savefile)
             sf_default= pickle.load(savefile)
+            sf_jkz= pickle.load(savefile)
             sf_samples= pickle.load(savefile)
             sf_zero= pickle.load(savefile)
             sf_drimmel= pickle.load(savefile)
@@ -72,7 +81,7 @@ def plot_effsel_location(location,plotname):
     else:
         xrange=[7.,15.8],
     bovy_plot.bovy_plot(distmods,sf_default,
-                        'k-',zorder=10,
+                        'b-',lw=_LW,zorder=10,
                         xrange=xrange,
                         yrange=[0.,1.1*numpy.amax(sf_zero)],
                         xlabel=r'$\mathrm{distance\ modulus}\ \mu$',
@@ -80,9 +89,17 @@ def plot_effsel_location(location,plotname):
     pyplot.fill_between(distmods,numpy.amin(sf_samples,axis=0),
                         numpy.amax(sf_samples,axis=0),color='0.4',
                         zorder=0)
-    bovy_plot.bovy_plot(distmods,sf_zero,'b-',overplot=True,zorder=7)
-    bovy_plot.bovy_plot(distmods,sf_drimmel,'y-',overplot=True,zorder=8)
-    bovy_plot.bovy_plot(distmods,sf_marshall,'r-',overplot=True,zorder=9)
+    bovy_plot.bovy_plot(distmods,sf_jkz,'g-.',lw=2.*_LW,
+                        overplot=True,zorder=11)
+    bovy_plot.bovy_plot(distmods,sf_zero,'k--',lw=_LW,overplot=True,zorder=7)
+    bovy_plot.bovy_plot(distmods,sf_drimmel,'y-',lw=_LW,overplot=True,zorder=8)
+    bovy_plot.bovy_plot(distmods,sf_marshall,'r-',lw=_LW,overplot=True,
+                        zorder=9)
+    # Label
+    lcen, bcen= apo.glonGlat(location)
+    if numpy.fabs(bcen) < 0.1: bcen= 0.
+    bovy_plot.bovy_text(r'$(l,b) = (%.1f,%.1f)$' % (lcen,bcen),
+                        top_right=True,size=16.)
     bovy_plot.bovy_end_print(plotname)
     return None
 
