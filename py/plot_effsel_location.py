d703dd91e5e6078981e10868ef8f3cf65191687c
Jo Bovy <jo.bovy@gmail.com>
2015-03-09 20:32:17 +0000

    apogee-maps: tweak

--- py/plot_effsel_location.py
+++ py/plot_effsel_location.py
@@ -141,7 +141,8 @@ def plot_effsel_location(location,plotname):
     if numpy.fabs(bcen) < 0.1: bcen= 0.
     bovy_plot.bovy_text(r'$(l,b) = (%.1f,%.1f)$' % (lcen,bcen),
                         top_right=True,size=16.)
-    bovy_plot.bovy_end_print(plotname)
+    bovy_plot.bovy_end_print(plotname,
+                             bbox_inches='tight')
     return None
 
 if __name__ == '__main__':
