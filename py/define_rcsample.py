7f196fca134d4e8dcf23e1d695409987db282484
Jo Bovy <jo.bovy@gmail.com>
2015-03-10 02:39:33 +0000

    apogee-maps: define the average alpha

--- py/define_rcsample.py
+++ py/define_rcsample.py
@@ -7,6 +7,7 @@ from galpy.util import bovy_coords
 import apogee.tools.read as apread
 from apogee.samples.rc import rcdist
 import isodist
+_FEHTAG= 'FE_H'
 def get_rcsample():
     """
     NAME:
@@ -45,5 +46,23 @@ def get_rcsample():
     data['RC_GALR_H']= R
     data['RC_GALPHI_H']= phi
     data['RC_GALZ_H']= Z
+    # Add the average alpha/Fe
+    data= esutil.numpy_util.add_fields(data,[('AVG_ALPHAFE', float)])
+    weight_o= numpy.ones(len(data))
+    weight_s= numpy.ones(len(data))
+    weight_si= numpy.ones(len(data))
+    weight_ca= numpy.ones(len(data))
+    weight_mg= numpy.ones(len(data))
+    weight_o[data['O_H'] == -9999.0]= 0.
+    weight_s[data['S_H'] == -9999.0]= 0.
+    weight_si[data['SI_H'] == -9999.0]= 0.
+    weight_ca[data['CA_H'] == -9999.0]= 0.
+    weight_mg[data['MG_H'] == -9999.0]= 0.
+    data['AVG_ALPHAFE']= (weight_o*data['O_H']+weight_s*data['S_H']
+                          +weight_si*data['SI_H']+weight_ca*data['CA_H']
+                          +weight_mg*data['MG_H'])/(weight_o+weight_s
+                                                    +weight_si+weight_ca
+                                                    +weight_mg)\
+                                                    -data['FE_H']
     return data
     
