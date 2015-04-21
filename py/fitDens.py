13f55c744c5607a782a674ade162c9ebf5d29a4b
Jo Bovy <jo.bovy@gmail.com>
2015-04-21 02:01:38 +0000

    apogee-maps: add range

--- py/fitDens.py
+++ py/fitDens.py
@@ -202,6 +202,7 @@ def _check_range_params_densfunc(params,type):
     if type.lower() == 'exp':
         if numpy.fabs(params[0]) > 2.: return False
         if numpy.fabs(params[1]) > 20.: return False
+        if numpy.exp(params[2]) > 1.: return False
     elif type.lower() == 'expplusconst':
         if numpy.fabs(params[0]) > 2.: return False
         if numpy.fabs(params[1]) > 20.: return False
