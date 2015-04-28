f419db7b7b8765706951d06e1bedc07dd13a94cb
Jo Bovy <jo.bovy@gmail.com>
2015-04-28 21:36:23 +0000

    apogee-maps: plot of first vs. second scale height

---
+++ py/plot_maptwohz.py
@@ -0,0 +1,47 @@
+###############################################################################
+# plot_maptwohz.py: make a plot of 2nd scale height vs. first scale height
+###############################################################################
+import sys
+import pickle
+import numpy
+import matplotlib
+matplotlib.use('Agg')
+from galpy.util import bovy_plot
+from matplotlib import pyplot
+import densprofiles
+import define_rcsample
+def plot_maphz(plotname):
+    # Load the two fit
+    with open('../mapfits/tribrokentwoexp.sav','rb') as savefile:
+        bf= numpy.array(pickle.load(savefile))
+        samples= numpy.array(pickle.load(savefile))
+    maps= define_rcsample.MAPs()
+    plotthisz1= numpy.zeros(len(bf))+numpy.nan
+    plotthisz1e= numpy.zeros(len(bf))+numpy.nan
+    plotthisz2= numpy.zeros(len(bf))+numpy.nan
+    plotthisz2e= numpy.zeros(len(bf))+numpy.nan
+    for ii, map in enumerate(maps.map()):
+        hzindx= densprofiles.ilogit(samples[ii,4]) > 0.15
+        tmed= numpy.nanmedian(1./samples[ii,1,hzindx])
+        terr= numpy.nanstd(1./samples[ii,1,hzindx])
+        plotthisz1[ii]= tmed
+        plotthisz1e[ii]= terr
+        tmed= numpy.nanmedian(1./samples[ii,5,hzindx])
+        terr= numpy.nanstd(1./samples[ii,5,hzindx])
+        plotthisz2[ii]= tmed
+        plotthisz2e[ii]= terr
+    plotthisz1[plotthisz1e/plotthisz1 > 0.5]= numpy.nan
+    bovy_plot.bovy_print()
+    bovy_plot.bovy_plot(plotthisz2*1000.,plotthisz1*1000.,'ko',
+                        xrange=[0.,1200.],yrange=[0.,1200.],
+                        xlabel=r'$\mathrm{2nd\ scale\ height\,(pc)}$',
+                        ylabel=r'$\mathrm{1st\ scale\ height\,(pc)}$',zorder=2)
+    bovy_plot.bovy_plot([0,1200],[0,1200],'k--',overplot=True,lw=2.)
+    pyplot.errorbar(plotthisz2*1000.,plotthisz1*1000.,
+                    yerr=plotthisz1e*1000.,
+                    marker='o',color='k',ls='none',zorder=1)
+    bovy_plot.bovy_end_print(plotname)
+    return None
+
+if __name__ == '__main__':
+    plot_maphz(sys.argv[1])
