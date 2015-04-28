b83d5fcf4b4aa4954c90ec048bfb26a2ea71b5c2
Jo Bovy <jo.bovy@gmail.com>
2015-04-28 23:09:40 +0000

    apoee-maps: make a plot of the radial profile

---
+++ py/plot_broadsurfdens.py
@@ -0,0 +1,83 @@
+###############################################################################
+# plot_broadsurfdens.py: make of plot of the surface-density of the broad bins
+###############################################################################
+import sys
+import pickle
+import numpy
+import matplotlib
+matplotlib.use('Agg')
+from galpy.util import bovy_plot
+from matplotlib import pyplot
+import densprofiles
+_SKIP= 1
+_SIGNIF= 0.025
+def plot_broadsurfdens(plotname):
+    broads= ['lowlow','solar','highfeh','highalpha']
+    hatchs= ['/','\\','//','\\\\']
+    anorms= [2.,.25,0.01,50.]
+    bovy_plot.bovy_print(fig_width=10.,fig_height=3.5)
+    overplot= False
+    for ii, broad in enumerate(broads):
+        # Restore the fits
+        savename= '../broadfits/%s.sav' % broad
+        with open(savename,'rb') as savefile:
+            bf_exp= pickle.load(savefile)
+            bf_brexp= pickle.load(savefile)
+            bf_twoexp= pickle.load(savefile)
+            ml_exp= pickle.load(savefile)
+            ml_brexp= pickle.load(savefile)
+            ml_twoexp= pickle.load(savefile)
+            samples_exp= pickle.load(savefile)
+            samples_brexp= pickle.load(savefile)
+            samples_twoexp= pickle.load(savefile)
+        # Create all density profiles
+        Rs= numpy.linspace(4.,14.,1001)
+        if broad.lower() == 'highalpha':
+            samples= samples_exp[:,::_SKIP]
+            nsamples= len(samples[0])
+            tRs= numpy.tile(Rs,(nsamples,1)).T
+            ihRin= numpy.tile(samples[0],(len(Rs),1))
+            ldp= -ihRin*(tRs-densprofiles._R0)
+        else:
+            samples= samples_brexp[:,::_SKIP]
+            nsamples= len(samples[0])
+            tRs= numpy.tile(Rs,(nsamples,1)).T
+            ldp= numpy.empty((len(Rs),nsamples))
+            Rb= numpy.tile(numpy.exp(samples[3]),(len(Rs),1))
+            ihRin= numpy.tile(samples[0],(len(Rs),1))
+            ihRout= numpy.tile(samples[2],(len(Rs),1))
+            ldp[tRs <= Rb]= ihRin[tRs<=Rb]*(tRs[tRs<=Rb]-densprofiles._R0)
+            ldp[tRs > Rb]= -ihRout[tRs>Rb]*(tRs[tRs>Rb]-densprofiles._R0)\
+                +ihRout[tRs>Rb]*(Rb[tRs>Rb]-densprofiles._R0)\
+                +ihRin[tRs>Rb]*(Rb[tRs>Rb]-densprofiles._R0)
+        norm= numpy.exp(numpy.median(ldp,axis=1))[numpy.argmin(numpy.fabs(Rs-densprofiles._R0))]/anorms[ii]        
+        bovy_plot.bovy_plot(Rs,numpy.exp(numpy.median(ldp,axis=1))/norm,
+                            'k-',
+                            lw=2.,overplot=overplot,
+                            xlabel=r'$R\,(\mathrm{kpc})$',
+                            ylabel=r'$\Sigma(R)$',
+                            xrange=[0.,16.],
+                            yrange=[0.0003,900.],
+                            semilogy=True)
+        pyplot.fill_between(Rs,
+                            numpy.exp(numpy.sort(ldp,axis=1)[:,int(round(_SIGNIF*nsamples))])/norm,
+                            numpy.exp(numpy.sort(ldp,axis=1)[:,int(round((1.-_SIGNIF)*nsamples))])/norm,
+#                            hatch=hatchs[ii],
+                            color='0.65',
+#                            facecolor=(0,0,0,0),
+                            lw=0.)
+        overplot= True
+    # Label
+    labelx= 1.
+    bovy_plot.bovy_text(labelx,0.01,r'$\mathrm{high\ [Fe/H]}$',
+                        size=15.,backgroundcolor='w')
+    bovy_plot.bovy_text(labelx,0.1,r'$\mathrm{solar}$',
+                        size=15.,backgroundcolor='w')
+    bovy_plot.bovy_text(labelx,0.6,r'$\mathrm{low\ [Fe/H]}$',
+                         size=15.,backgroundcolor='w')
+    bovy_plot.bovy_text(labelx,300.,r'$\mathrm{high}\ [\alpha/\mathrm{Fe}]$',
+                         size=15.,backgroundcolor='w')
+    bovy_plot.bovy_end_print(plotname)
+
+if __name__ == '__main__':
+    plot_broadsurfdens(sys.argv[1])
