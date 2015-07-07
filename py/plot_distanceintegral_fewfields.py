2e7d1f7e1ed8c5ba3ccf3f102f2641ed40af16b6
Jo Bovy <jo.bovy@gmail.com>
2015-07-07 22:06:05 +0000

    apogee-maps: add APOGEE option

--- py/plot_distanceintegral_fewfields.py
+++ py/plot_distanceintegral_fewfields.py
@@ -7,13 +7,11 @@ import sys
 import os, os.path
 import pickle
 import numpy
-import h5py
 from scipy import signal, integrate, interpolate
 import healpy
 import matplotlib
 matplotlib.use('Agg')
-from galpy.util import save_pickles, bovy_plot, multi
-import multiprocessing
+from galpy.util import save_pickles, bovy_plot
 import densprofiles
 import dust
 import gaia_rc
@@ -23,23 +21,36 @@ _GMAX= 20.
 _NGSAMPLES= 10000
 _NSIDE= 2048
 _DEGTORAD= numpy.pi/180.
-def plot_distanceintegral_fewfields(savename,plotname):
+def plot_distanceintegral_fewfields(savename,plotname,apogee=False):
     #ls and bs of the patches
     ls= [280.,30.,12.,37.5,90.,127.5,30.,60.]
     bs= [45.,4.,2.,0.,0.,0.,0.,0.]
     radius= 1.49
+    locs= [4214,4244,4378,4312,4242,4318,4240,4241] # for apogee
+    # If we are doing APOGEE selection, load the APOGEE selection function
+    if apogee:
+        selectFile= '../savs/selfunc-nospdata.sav'
+        if os.path.exists(selectFile):
+            with open(selectFile,'rb') as savefile:
+                apo= pickle.load(savefile)
     if os.path.exists(savename):
         with open(savename,'rb') as savefile:
             area= pickle.load(savefile)
+            if apogee:
+                area_cdens= pickle.load(savefile)
     else: 
-        # For samping over the absolute magnitude distribution
-        iso= gaia_rc.load_iso()
-        Gsamples= gaia_rc.sample_Gdist(iso,n=_NGSAMPLES)
+        if not apogee:
+            # For samping over the absolute magnitude distribution
+            iso= gaia_rc.load_iso()
+            Gsamples= gaia_rc.sample_Gdist(iso,n=_NGSAMPLES)
         # Loop through each distance slice
         area= numpy.zeros((len(ls),len(dust._GREEN15DISTS)))
+        if apogee:
+            area_cdens= numpy.zeros((len(ls),len(dust._GREEN15DISTS)))
         for ii,dist in enumerate(dust._GREEN15DISTS):
             print "Working on distance %i: %f" % (ii,dist)
             G0= 0.68+dust.dist2distmod(dist)
+            H0= -1.49+dust.dist2distmod(dist)
             # Load the dust map
             combinedmap= dust.load_combined(dist,nest=False,nside_out=_NSIDE)
             # Loop through a few patches
@@ -62,12 +73,27 @@ def plot_distanceintegral_fewfields(savename,plotname):
                 combinedmask= numpy.zeros(len(ipixs))
                 tcombinedmap= combinedmap[ipixIndx]
                 for jj in range(_NGSAMPLES):
-                    combinedmask+= ((tcombinedmap > (_GMIN-G0-Gsamples[jj]+0.68))\
-                            *(tcombinedmap < (_GMAX-G0-Gsamples[jj]+0.68))).astype('float')
+                    if apogee:
+                        # Loop through the three cohorts
+                        for c in ['short','medium','long']:
+                            hmin= apo.Hmin(locs[jj],cohort=c)
+                            hmax= apo.Hmax(locs[jj],cohort=c)
+                            combinedmask+= ((tcombinedmap > (hmin-H0))\
+                                                *(tcombinedmap < (hmax-H0))).astype('float')*apo(locs[jj],hmin+0.01)
+                    else:
+                        combinedmask+= ((tcombinedmap > (_GMIN-G0-Gsamples[jj]+0.68))\
+                                            *(tcombinedmap < (_GMAX-G0-Gsamples[jj]+0.68))).astype('float')
                 combinedmask/= _NGSAMPLES
                 # Compute cross correlation
                 area[ff,ii]= float(numpy.sum(cosb[ipixIndx]*densmap\
                                                  *combinedmask))
+                if apogee:
+                    # Also store the approximation that the density is constant
+                    cdens= densprofiles.expdisk(ll*_DEGTORAD,bb*_DEGTORAD,dist,
+                                                glon=True,
+                                                params=[1./3.,1./0.3])
+                    area_cdens[ff,ii]= float(numpy.sum(cosb[ipixIndx]*cdens\
+                                                           *combinedmask))
         save_pickles(savename,area)
     # Plot the power spectrum
     if True:
@@ -107,5 +133,5 @@ def plot_distanceintegral_fewfields(savename,plotname):
 
 if __name__ == '__main__':
     plot_distanceintegral_fewfields(sys.argv[1], # savefilename
-                                     sys.argv[2]) # plotfilename
-    
+                                    sys.argv[2], # plotfilename
+                                    len(sys.argv) > 3) # APOGEE selection?
