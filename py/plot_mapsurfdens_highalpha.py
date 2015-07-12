9a0e9580d13dea61d6a27727b6a57a5bf7669d39
Jo Bovy <jo.bovy@gmail.com>
2015-07-12 00:12:37 +0000

    apogee-maps: figure 10 for new feh calibration

--- py/plot_mapsurfdens_highalpha.py
+++ py/plot_mapsurfdens_highalpha.py
@@ -49,18 +49,18 @@ def plot_mapsurfdens(plotname):
         ldp[gtRb]= -ihRout[gtRb]*(tRs[gtRb]-densprofiles._R0)
         # Label and relative normalization
         tfeh= round(numpy.median(map['FE_H'])*20.)/20.
-        if tfeh == 0.35: tfeh= 0.4
-        if tfeh == -0.0: tfeh= 0.0
+        if tfeh == 0.25: tfeh= 0.3
+        if tfeh == -0.1: tfeh= -0.1
         print ii, tfeh, len(map)
-        anorm= 10**(-10.*(-tfeh-0.4))
+        anorm= 10**(-10.*(-tfeh-0.5))
         #if (-tfeh-0.4) > 0.3: anorm= 10**(-12.*(-tfeh+0.4)) 
-        if (-tfeh-0.4) == -0.2: anorm= 10**(-11.*(-tfeh-0.4))
+        if (-tfeh-0.5) == -0.2: anorm= 10**(-11.*(-tfeh-0.5))
         anorm= 1./anorm # re-order
         anorm/= 3.
         norm= numpy.exp(numpy.median(ldp,axis=1))[numpy.argmin(numpy.fabs(Rs-densprofiles._R0))]/anorm
         bovy_plot.bovy_plot(Rs,numpy.exp(numpy.median(ldp,axis=1))/norm,
                             '-',
-                            color=cmap((tfeh+0.4)*0.95/0.5+0.05),
+                            color=cmap((tfeh+0.5)*0.95/0.5+0.05),
                             lw=2.,overplot=overplot,
                             ylabel=r'$\Sigma(R)\times\mathrm{constant}$',
                             xrange=[0.,16.],
@@ -69,7 +69,7 @@ def plot_mapsurfdens(plotname):
         pyplot.fill_between(Rs,
                             numpy.exp(numpy.sort(ldp,axis=1)[:,int(round(_SIGNIF*nsamples))])/norm,
                             numpy.exp(numpy.sort(ldp,axis=1)[:,int(round((1.-_SIGNIF)*nsamples))])/norm,
-                            color=cmap((tfeh+0.4)/0.4),
+                            color=cmap((tfeh+0.5)/0.4),
                             lw=0.)
         overplot= True
         if ii == 19:
@@ -78,7 +78,7 @@ def plot_mapsurfdens(plotname):
                                 r'$[\mathrm{Fe/H}]$',size=16.,color='k')
         bovy_plot.bovy_text(2.,(numpy.exp(numpy.median(ldp,axis=1))/norm)[0],
                             r'$%+.1f$' % tfeh,size=16.,
-                            color=cmap((tfeh+0.4)*0.95/0.5+0.05))
+                            color=cmap((tfeh+0.5)*0.95/0.5+0.05))
     bovy_plot.bovy_text(10.,10.**1.,
                         r'$\mathrm{high-}[\alpha/\mathrm{Fe}]\ \mathrm{MAPs}$',
                         size=16.)
