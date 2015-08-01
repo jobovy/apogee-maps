3522e1cdd5d8e85fdda867e9bc8dba27ecef589e
Jo Bovy <jo.bovy@gmail.com>
2015-08-01 23:38:22 +0000

    apogee-maps: label dust maps

--- py/plot_dust_gaia.py
+++ py/plot_dust_gaia.py
@@ -6,7 +6,7 @@ import numpy
 import healpy
 import matplotlib
 matplotlib.use('Agg')
-from galpy.util import bovy_plot
+from galpy.util import bovy_plot, bovy_coords
 import dust
 # nside to work at, 2048 is the max
 _NSIDE= 2048
@@ -28,6 +28,32 @@ def plot_dust_gaia(dist,plotname):
                              cmap='gist_yarg',
                              title="",
                              unit='$A_G\,(\mathrm{mag})$')
+    # Plot outline of Marshall et al.
+    ls= numpy.linspace(-100.125,100.125,1001)
+    healpy.visufunc.projplot(ls,ls*0.-10.125,'k--',lw=1.2,lonlat=True)
+    healpy.visufunc.projplot(ls,ls*0.+10.125,'k--',lw=1.2,lonlat=True)
+    bs= numpy.linspace(-10.125,10.125,1001)
+    healpy.visufunc.projplot(bs*0.-100.125,bs,'k--',lw=1.2,lonlat=True)
+    healpy.visufunc.projplot(bs*0.+100.125,bs,'k--',lw=1.2,lonlat=True)
+    # Plot boundary of Green et al. map, dec > -30.
+    ras= numpy.linspace(0.,360.,1001)
+    decs= ras*0.-30.
+    lbs= bovy_coords.radec_to_lb(ras,decs,degree=True)
+    ls= lbs[:,0]
+    bs= lbs[:,1]
+    # rm those within Marshall et al.
+    keepIndx= True-((ls > 259.875)*(numpy.fabs(bs) < 10.125)
+                    +(ls < 100.125)*(numpy.fabs(bs) < 10.125))
+    ls= ls[keepIndx]
+    bs= bs[keepIndx]
+    healpy.visufunc.projplot(ls,bs,'k-.',lw=1.2,lonlat=True)
+    # Labels
+    healpy.visufunc.projtext(350.,-8.,r'$\mathrm{Marshall\ et\ al.\ (2006)}$',
+                             lonlat=True,size=13.)
+    healpy.visufunc.projtext(10.,-60.,r'$\mathrm{Drimmel\ et\ al.\ (2003)}$',
+                             lonlat=True,size=13.)
+    healpy.visufunc.projtext(160.,40.,r'$\mathrm{Green\ et\ al.\ (2015)}$',
+                             lonlat=True,size=13.)
     bovy_plot.bovy_end_print(plotname)
 
 if __name__ == '__main__':
